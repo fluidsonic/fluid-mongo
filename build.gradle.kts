4b7aa791cb92abb7d9c460dfa7bc2797b67f0ae0
Marc Knaup <marc@knaup.io>
2020-01-24 18:29:33 +0700

    🎉 1.0.0-beta.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.fluidsonic.gradle") version "1.0.6"
 }
 
-fluidJvmLibrary(name = "mongo", version = "1.0.0-beta.2")
+fluidJvmLibrary(name = "mongo", version = "1.0.0-beta.3")
 
 fluidJvmLibraryVariant(JvmTarget.jdk8) {
 	description = "Kotlin coroutine support for MongoDB built on top of the official MongoDB Asynchronous Java Driver"
