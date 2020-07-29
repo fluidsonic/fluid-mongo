d79eef321524f8cd98c84711cf1f088a4b6b5a1b
Marc Knaup <marc@knaup.io>
2020-07-29 22:25:14 +0100

    🎉 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 	id("io.fluidsonic.gradle") version "1.1.0"
 }
 
-fluidLibrary(name = "mongo", version = "1.0.1")
+fluidLibrary(name = "mongo", version = "1.1.0")
 
 fluidLibraryModule(description = "Kotlin coroutine support for MongoDB built on top of the official Reactive Streams Java Driver") {
 	publishSingleTargetAsModule()
