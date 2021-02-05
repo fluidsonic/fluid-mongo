f9fed51b3f96e14e7633bbe66c99800004e0486b
Marc Knaup <marc@knaup.io>
2021-02-05 01:17:56 +0100

    Updated all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
 import io.fluidsonic.gradle.*
 
 plugins {
-	id("io.fluidsonic.gradle") version "1.1.16"
+	id("io.fluidsonic.gradle") version "1.1.18"
 }
 
 fluidLibrary(name = "mongo", version = "1.1.3")
 
 fluidLibraryModule(description = "Kotlin coroutine support for MongoDB built on top of the official Reactive Streams Java Driver") {
-	publishSingleTargetAsModule()
-
 	targets {
 		jvm {
 			dependencies {
